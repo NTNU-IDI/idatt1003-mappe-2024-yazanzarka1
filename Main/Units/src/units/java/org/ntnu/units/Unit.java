9e8b3b1d73fd78ea6bace810dc20d96181ab494a
yazanzarka1 <yazanzarka1@gmail.com>
2024-10-02 17:07:28 +0200

    Add module configuration files for Main, Food, and Units

--- Main/Units/src/units/java/org/ntnu/units/Unit.java
+++ Main/Units/src/units/java/org/ntnu/units/Unit.java
@@ -1,21 +1,42 @@
 package org.ntnu.units;
-
+import org.
 public abstract class Unit {
+    // unit name - KG, Liter
     String unitName;
     float value;
 
+    /**
+     * getter for unit value
+     * @return float
+     */
     public float getValue(){
         return value;
     }
 
+    /**
+     * getter for unitName: KG, Liter
+     * @return
+     */
     public String getUnitName() {
         return unitName;
     }
 
+    /**
+     * adds to units' value
+     * @param Unit otherUnit
+     */
     public void plus(Unit otherUnit){
+
+        if(this.unitName == otherUnit.unitName){
+            throw new IllegalArgumentException()
+        }
         this.value += otherUnit.value;
     }
 
+    /**
+     * substracts from unit's value
+     * @param Unit otherUnit
+     */
     public void subtract(Unit otherUnit){
         this.value -= otherUnit.value;
     }
